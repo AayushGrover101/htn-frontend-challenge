3ffef53ffee7e0989385b776049075a1f540fd0b
Aayush Grover <aayushgrover@eduroam-campus-10-36-142-113.campus-dynamic.uwaterloo.ca>
2025-02-13 00:10:01 -0500

    Small Margin Fixes + Added Site Icon

--- src/components/SignInButton.jsx
+++ src/components/SignInButton.jsx
@@ -14,7 +14,7 @@ function SignInButton({ onSignInClick }) {
 
   return (
     <>
-      <button onClick={handleClick} className={`sm:px-5 px-5 xs:ml-3 pt-1.5 pb-1.5 border-[1px] sm:text-sm text-xs rounded-lg border-white bg-opacity-20 bg-transparent text-white transition-all outline-none ${isAuthenticated ? 'hover:bg-opacity-100 hover:bg-white hover:text-background' : 'hover:bg-opacity-100 hover:text-white hover:bg-primary hover:border-primary hover:shadow-[0_0_20px_rgba(236,72,153,0.5)]'}`}>
+      <button onClick={handleClick} className={`sm:px-5 px-5 xs:ml-3 ml-1.5 pt-1.5 pb-1.5 border-[1px] sm:text-sm text-xs rounded-lg border-white bg-opacity-20 bg-transparent text-white transition-all outline-none ${isAuthenticated ? 'hover:bg-opacity-100 hover:bg-white hover:text-background' : 'hover:bg-opacity-100 hover:text-white hover:bg-primary hover:border-primary hover:shadow-[0_0_20px_rgba(236,72,153,0.5)]'}`}>
         {isAuthenticated ? 'LOG OUT' : 'SIGN IN'}
       </button>
     </>
